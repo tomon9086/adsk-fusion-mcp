2b8e5833d3d448037192bd9016d28b502c5b0644
tomon9086 <tomon9086@gmail.com>
2025-09-28 01:51:09 +0900

    initial commit

---
+++ addin/FusionMCPAddIn.py
@@ -0,0 +1,29 @@
+import traceback
+
+import adsk.core
+
+
+def run(context):
+    global app
+    global ui
+    
+    try:
+        app = adsk.core.Application.get()
+        ui = app.userInterface
+
+        ui.messageBox('Hello, World!')
+
+    except:
+        if ui:
+            ui.messageBox('ERROR:\n{}'.format(traceback.format_exc()))
+
+def stop(context):
+    global ui
+    
+    try:        
+        ui.messageBox('Stopped!')
+        
+    except:
+        if ui:
+            ui.messageBox('ERROR:\n{}'.format(traceback.format_exc())) 
+            
