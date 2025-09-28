676eff1183e4b332908b2a5b40d3ba4dbfa5dece
tomon9086 <tomon9086@gmail.com>
2025-09-28 10:48:26 +0900

    refactor: format code

--- addin/FusionMCPAddIn.py
+++ addin/FusionMCPAddIn.py
@@ -6,24 +6,24 @@ import adsk.core
 def run(context):
     global app
     global ui
-    
+
     try:
         app = adsk.core.Application.get()
         ui = app.userInterface
 
-        ui.messageBox('Hello, World!')
+        ui.messageBox("Hello, World!")
 
     except:
         if ui:
-            ui.messageBox('ERROR:\n{}'.format(traceback.format_exc()))
+            ui.messageBox("ERROR:\n{}".format(traceback.format_exc()))
+
 
 def stop(context):
     global ui
-    
-    try:        
-        ui.messageBox('Stopped!')
-        
+
+    try:
+        ui.messageBox("Stopped!")
+
     except:
         if ui:
-            ui.messageBox('ERROR:\n{}'.format(traceback.format_exc())) 
-            
+            ui.messageBox("ERROR:\n{}".format(traceback.format_exc()))
